7d8996784699c27dbfb77ac5162f258dc4f73773
Nil Silva <nil_silva_12@hotmail.com>
2019-03-20 21:58:55 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/mensagem/MainActivity.java
+++ app/src/main/java/com/example/mensagem/MainActivity.java
@@ -2,17 +2,12 @@ package com.example.mensagem;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
